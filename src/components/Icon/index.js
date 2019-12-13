52a3cbec55baabaaf494a218c54910c17793418c
Lucas Craveiro Paes <lucascraveiropaes@gmail.com>
2019-12-13 15:56:18 -0300

    Adicionando novos ícones

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,9 +2,22 @@ import React, { Component } from "react";
 import icons                from "./icons/";
 import "./styles.scss";
 
+const excelReports = ["txt-profile", "municipio-profile", "iddf", "base-aneis", "base-circuitos", "base-enlaces"];
+
 class Icon extends Component {
     render() {
-        const { name, size, style } = this.props;
+        let { name, size, style } = this.props;
+        name = name.toLowerCase();
+
+        if (!icons[name]) {
+            if (name.search("listar") > -1) {
+                name = "listar";
+            } else if (name.search("carga") > -1 || name.search("massiva") > -1) {
+                name = "carga";
+            } else if (excelReports.includes(name)) {
+                name = "relatorios-excel";
+            }
+        }
 
         return (
             <img src={ icons[name] } className="dash-icon" style={{ width: size, height: size, ...style }} alt={ name }/>
--- src/components/ModuleCard/index.js
+++ src/components/ModuleCard/index.js
@@ -4,21 +4,81 @@ import { MenuButton, Icon } from "components";
 import data                 from "data.js";
 import "./styles.scss";
 
+const pageName= "iOS Folder Animation";
+
 class ModuleCard extends Component {
     constructor(props) {
         super(props);
 
+        this.menus = data.filter(el => el.parent_id === props.item.id);
+
         this.state = {
             index: 0,
-            menu: 0,
+            menu: -1,
         }
     }
 
+    menus = []
+
+    onEscPress = e => {
+        if(e.keyCode === 27) { // If user press esc
+            this.folder.classList.remove("open");
+            setTimeout(this.resetState, 500);
+        }
+    }
+
+    resetState = () => this.setState({ index: 0, menu: -1 });
+
+    getURLParams = () => {
+        const url = window.location.href.split("?")[1];
+        const params = {};
+
+        if (url) {
+            const paramsArr = url.split("&");
+
+            for (let param of paramsArr) {
+                const arr = param.split("=");
+                params[arr[0]] = arr[1];
+            }
+        }
+
+        return params;
+    }
+
+    componentDidMount = () => {
+        document.addEventListener("keydown", this.onEscPress, false);
+
+        const params = this.getURLParams();
+        let folder = null;
+
+        if (params.folder) {
+            if (this.folder.id === params.folder) {
+                this.folder.classList.toggle("open");
+            }
+        }
+    }
+
+    componentWillUnmount = () => document.removeEventListener("keydown", this.onEscPress, false);
+
     toggleFolder = () => {
+        if (this.state.index !== 0)
+            return this.setState({ index: 0 });
+
+            console.log(this.folder.id);
+
+        if (this.folder.classList.contains("open") === false) {
+            const menu = data.find(el => el.id === this.props.item.id);
+            const path = menu.path.replace("/", "");
+
+            window.history.pushState({
+                id: path
+            }, pageName, `${window.location.href}?folder=${path}`);
+        } else {
+            window.history.pushState(null, "", window.location.href.replace(`?folder=${this.folder.id}`, ""));
+        }
+
         this.folder.classList.toggle("open");
-        setTimeout(() => {
-            this.setState({ index: 0 })
-        }, 500)
+        setTimeout(this.resetState, 500);
     }
 
     onMenuClick = i => this.setState({ index: 1, menu: i.id });
@@ -28,21 +88,21 @@ class ModuleCard extends Component {
     render() {
         const { index, menu } = this.state;
         const { item } = this.props;
-        const menus = data.filter(el => el.parent_id === item.id);
+        const path = item.path.replace("/", "");
 
         return (
             <div className="button-container">
                 <button className="module-button" onClick={ this.toggleFolder }>
-                    <Icon name={ item.path.replace("/", "") } size={ 60 }/>
+                    <Icon name={ path } size={ 60 }/>
                     <span>{ item.name }</span>
                 </button>
 
-                <div ref={ ref => this.folder = ref } className="folder">
+                <div ref={ ref => this.folder = ref } className="folder" id={ path }>
                     <div className="overlay" onClick={ this.toggleFolder }></div>
                     <div className="container">
-                        <Carousel slideIndex={ index } withoutControls={ true } swiping={ false } disableEdgeSwiping={ true } dragging={ false } pauseOnHover={ false }>
+                        <Carousel speed={ 250 } slideIndex={ index } withoutControls={ true } swiping={ false } disableEdgeSwiping={ true } dragging={ false } pauseOnHover={ false }>
                             <div className="menu-list">
-                                { menus.map((i, k) =>
+                                { this.menus.map((i, k) =>
                                     <MenuButton k={`menu_${k*i.id}_${i.id}`} menu={ i } onClick={ this.onMenuClick }/>
                                 )}
                             </div>
